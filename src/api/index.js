c38c3dda149a4af72bee22caefc1d128868053db
yasirw212 <williamsyasir90@gmail.com>
2023-12-17 21:29:05 -0600

    API call
    Updating call to api to receive product information from database.

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 axios.defaults.withCredentials = true
 
-// export const getAllProducts = () => axios.get("https://ownxmind-backend-fe62091fcc52.herokuapp.com/products")
+export const getAllProducts = () => axios.get("https://ownxmind-backend-fe62091fcc52.herokuapp.com/products")
 
 export const getAllPhotos = () => axios.get("https://ownxmind-backend-fe62091fcc52.herokuapp.com/photos")
 
