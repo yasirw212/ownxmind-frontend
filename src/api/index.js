793c3d459ca6ace526cb656e1a9ff8e5f5595fc5
yasirw212 <williamsyasir90@gmail.com>
2024-01-03 08:21:35 -0600

    Server calls

--- src/api/index.js
+++ src/api/index.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 axios.defaults.withCredentials = true
 
-// export const getAllProducts = () => axios.get("https://ownxmind-backend-fe62091fcc52.herokuapp.com/products")
-export const getAllProducts = () => axios.get('http://localhost:4000/products')
+export const getAllProducts = () => axios.get("https://ownxmind-backend-fe62091fcc52.herokuapp.com/products")
+// export const getAllProducts = () => axios.get('http://localhost:4000/products')
 
 export const getAllPhotos = () => axios.get("https://ownxmind-backend-fe62091fcc52.herokuapp.com/photos")
 // export const getAllPhotos = () => axios.get('http://localhost:4000/photos')
 
-// export const retrieveSessionData = () => axios.get('https://ownxmind-backend-fe62091fcc52.herokuapp.com/order')
-export const retrieveSessionData = () => axios.get('http://localhost:4000/order')
+export const retrieveSessionData = () => axios.get('https://ownxmind-backend-fe62091fcc52.herokuapp.com/order')
+// export const retrieveSessionData = () => axios.get('http://localhost:4000/order')
 
 
-// export const updateSessionData = (data) => axios.post("https://ownxmind-backend-fe62091fcc52.herokuapp.com/order", [...data])
-export const updateSessionData = (data) => axios.post("http://localhost:4000/order", [...data])
+export const updateSessionData = (data) => axios.post("https://ownxmind-backend-fe62091fcc52.herokuapp.com/order", [...data])
+// export const updateSessionData = (data) => axios.post("http://localhost:4000/order", [...data])
